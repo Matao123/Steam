fab3d9333744dbaa10501cb100eec629c6119450
Matao123 <1580719765@qq.com>
2018-02-26 10:13:33 +0800

    2月26日提交

--- Steam.js
+++ Steam.js
@@ -1,210 +1,233 @@
-// 侧边栏小雪碧图
+		// ----------------------------侧边栏雪碧图逻辑-----------------------	
+
+		// 侧边栏小雪碧图
 		var $iLsit = $(".gutter2").find("i")
 		// 枚举背景图数
 		for(var i = 0;i<$iLsit.length;i++){
 			// 判断position值
 			$iLsit[i].style.backgroundPosition = -(i*16)+"px"
 		}
-		
-		// 轮播图逻辑
-		var isAnimation = false
-		var arr = [0,0,0,0]
-		var n = 0
+			
 
-		// 分装生成小圆点函数
-							// 图片个数 小圆点父级
-		function genericPoint(photos,speckParent){
-			for(var i = 0;i<photos;i++){
-				var $newSpan = $("<span></span>")
-				$newSpan.attr("index",i)
-				speckParent.append($newSpan)
+		// ------------------------------轮播图逻辑---------------------------
+
+		// 轮播图函数
+		var isAnimation = false
+
+		// 自动生成小圆点
+		for(var i = 0; i < $(".carousel_container").length;i++){
+			//  count为每个轮播图的图片数
+			var count = $(".carousel_container").eq(i).find(".scheme").length;
+			// 枚举每个轮播图的图片数
+			for(var j = 0; j < count;j++){
+				//  生成小圆点
+				var $obj = $("<span></span>")
+				//  插入每个小圆点的父级里
+				$obj.appendTo($(".dot").eq(i))
+				if(j == 0){
+					// 小圆点矫正位置
+					$obj.addClass("focus")
+				}
 			}
 		}
 
-		// 分装小圆点点击函数
-							// 小圆点 图片对象
-		function clickOnthedot(speck,object){
-			speck.click(function(){
-	        	if(!isAnimation){
-	          		isAnimation = true;
-	          		n = $(this).attr("index")
-	          		object.hide()
-      				speck.removeClass("focus")
-      				speck.eq(n).addClass("focus")
-      				object.eq(n).fadeIn(500)
-	        	}
-	        	isAnimation = false
-	      	})
+		// 初始化索引值
+		for(var i = 0; i < $(".carousel_container").length;i++){
+			//  每个轮播图里的大图片
+			var $liList = $(".carousel_container").eq(i).find(".scheme");
+			//  每个轮播图里的小圆点
+			var $spanList = $(".carousel_container").eq(i).find(".dot").find("span");
+			//  枚举每个轮播图的大图片数
+			for(var j = 0; j < $liList.length; j++){
+				//  大图片插入非法数字
+				$liList.eq(j).attr("index",j)
+				//  小圆点插入非法数字
+				$spanList.eq(j).attr("index",j)
+			}
 		}
 
-      	// 分装向右点击函数
-      				// 	右按钮 图片对象 小圆点
-      	function right(right,object,speck){
-      		right.click(function(){
-      			if(!isAnimation){
-      				isAnimation = true
-      				object.hide()
-      				speck.removeClass("focus")
-      				if(n < object.length-1){
-      					n++
-      				}
-      				else{
-      					n=0
-      				}
-      				speck.eq(n).addClass("focus")
-      				object.eq(n).fadeIn(400)
-      			}
-      			isAnimation = false
-      		})
-      	}
+		// 分装改变图片的函数
+		function changeImg(index,obj){
+			var $navList = $(obj).closest(".carousel_container").find(".dot").find("span");
+			// 隐藏所有的图片
+			$liList.hide();
+			$liList.removeClass("focus");
+			$navList.removeClass("focus");
+			// 矫正图片位置，正确位置图片显示
+			$liList.eq(index).addClass("focus")
+			$liList.eq(index).fadeIn();
+			$navList.eq(index).addClass("focus")
+		}
 
-      	// 分装向左点击函数
-      				// 	左按钮 图片对象 小圆点
-      	function left(left,object,speck){
-			left.click(function(){
-				if(!isAnimation){
-					isAnimation = true
-					object.hide()
-      				speck.removeClass("focus")
-					if(n == 0){
-						n = object.length-1
+		//  右边点击事件
+		$(".right").click(function(){
+			if(!isAnimation){
+				// 当前点击的轮播图里的大图片
+				$liList = $(this).closest(".carousel_container").find(".scheme");
+				// 枚举大图片数
+				for(var i = 0; i < $liList.length; i++){
+					if($liList.eq(i).hasClass("focus")){
+						isAnimation = true
+						// 判断图片当前位置
+						if(i < $liList.length - 1){
+							changeImg(i+1,$(this))
+						}
+						else{
+							changeImg(0,$(this))
+						}
+						break;
+					}
+				}	
+			}
+			isAnimation = false
+		})
+
+		//  左边点击事件
+		$(".left").click(function(){
+			if(!isAnimation){
+				// 当前点击的轮播图里的大图片
+				$liList = $(this).closest(".carousel_container").find(".scheme");
+				// 枚举大图片数
+				for(var i = 0; i < $liList.length; i++){
+					if($liList.eq(i).hasClass("focus")){
+						isAnimation = true
+						// 判断图片当前位置
+						if(i !== 0){
+							changeImg(i-1,$(this))
+						}
+						else{
+							changeImg($liList.length - 1,$(this))
+						}
+						break;
+					}
+				}
+			}
+			isAnimation = false
+		})
+
+		//  小圆点点击事件
+		$(".dot span").click(function(){
+			if(!isAnimation){
+				// 当前点击的轮播图里的大图片
+				$liList = $(this).closest(".carousel_container").find(".scheme");
+				//  变量n为当前点击小圆点的非法数字
+				var n = this.getAttribute("index");
+				// 枚举大图片数
+				for(var i = 0; i < $liList.length; i++){
+					if($liList.eq(i).hasClass("focus")){
+						isAnimation = true
+						changeImg(n,$(this))
+						break;
+					}
+				}
+			}
+			isAnimation = false
+		})
+
+
+		// ---------------------------自动播放第一个轮播图---------------------
+
+		//  分装自动播放轮播图函数
+		function autoPlay(){
+			// 第一个轮播图里的大图片
+			$liList = $(".carousel_container").eq(0).find(".scheme");
+			// 枚举大图片数
+			for(var i = 0; i < $liList.length; i++){
+				// 判断图片当前位置
+				if($liList.eq(i).hasClass("focus")){
+					if(i < $liList.length - 1){
+						changeImg(i+1,$(".carousel_container").eq(0).find(".right"))
 					}
 					else{
-						n--
+						changeImg(0,$(".carousel_container").eq(0).find(".right"))
 					}
-					speck.eq(n).addClass("focus")
-      				object.eq(n).fadeIn(400)
+					break;
 				}
-				isAnimation = false
-			})
-      	}
-
-      	// 分装自动播放函数
-      	function rightAuto(object,speck){
-      		if(!isAnimation){
-      				isAnimation = true
-      				object.hide()
-      				speck.removeClass("focus")
-      				if(n < object.length-1){
-      					n++
-      				}
-      				else{
-      					n=0
-      				}
-      				speck.eq(n).addClass("focus")
-      				object.eq(n).fadeIn(400)
-      			}
-      			isAnimation = false
-      	}
+			}
+		}
+		var auto = setInterval(function(){
+			autoPlay()
+		},3000)
+		$(".carousel_container").eq(0).mouseenter(function(){
+			clearInterval(auto)
+		}).mouseleave(function(){
+			auto = setInterval(function(){
+				autoPlay()
+			},3000)
+		})
 
 
-      	// 调用函数给第一个轮播图
-      	genericPoint($("#chart1 .scheme").length,$("#chart1 .dot"))
-      	clickOnthedot($("#chart1 .dot span"),$("#chart1 .scheme"))
-      	right($("#chart1 .right"),$("#chart1 .scheme"),$("#chart1 .dot span"))
-      	left($("#chart1 .left"),$("#chart1 .scheme"),$("#chart1 .dot span"))
-      	$("#chart1 .scheme").eq(0).show()
-      	$("#chart1 .dot span").eq(0).addClass("focus")
-
-      	// 调用函数给第二个轮播图
-      	genericPoint($("#chart2 .scheme").length,$("#chart2 .dot"))
-      	clickOnthedot($("#chart2 .dot span"),$("#chart2 .scheme"))
-      	right($("#chart2 .right"),$("#chart2 .scheme"),$("#chart2 .dot span"))
-      	left($("#chart2 .left"),$("#chart2 .scheme"),$("#chart2 .dot span"))
-      	$("#chart2 .scheme").eq(0).show()
-      	$("#chart2 .dot span").eq(0).addClass("focus")
-
-      	// 调用函数给第三个轮播图
-      	genericPoint($("#chart3 .curator_page").length,$("#chart3 .dot"))
-      	clickOnthedot($("#chart3 .dot span"),$("#chart3 .curator_page"))
-      	right($("#chart3 .right"),$("#chart3 .curator_page"),$("#chart3 .dot span"))
-      	left($("#chart3 .left"),$("#chart3 .curator_page"),$("#chart3 .dot span"))
-      	$("#chart3 .curator_page").eq(0).show()
-      	$("#chart3 .dot span").eq(0).addClass("focus")
-
-      	// 调用函数给第四个轮播图
-      	genericPoint($("#chart4 .cheap").length,$("#chart4 .dot"))
-      	clickOnthedot($("#chart4 .dot span"),$("#chart4 .cheap"))
-      	right($("#chart4 .right"),$("#chart4 .cheap"),$("#chart4 .dot span"))
-      	left($("#chart4 .left"),$("#chart4 .cheap"),$("#chart4 .dot span"))
-      	$("#chart4 .cheap").eq(0).show()
-      	$("#chart4 .dot span").eq(0).addClass("focus")
-
-      	// table选项卡逻辑
-      	var $merchandise = $(".merchandise").find("li")
-      	var $home_leftcol = $(".home_leftcol")
-      	var $tab_nav = $(".tab_nav")
-      	$tab_nav.eq(0).addClass("prev")
-      	var a= 0
+  		// ---------------------------table选项卡逻辑---------------------------
+      	
+      	var suo = false
+      	var a = 0
       	// 鼠标移上事件
-      	$home_leftcol.mouseenter(function(){
-      		var $aLeft = $(this).children("a")
-      		var $preview = $(this).next().find(".tab_preview")
-	  		// 枚举(游戏)个数
-	      	for(var i = 0;i<$aLeft.length;i++){
-	      		// 插入非法数字
-	      		$aLeft.eq(i).attr("index",i)
-		    }
-		  	// 鼠标移上事件
-		  	$aLeft.mouseenter(function(){
-	      		$aLeft.removeClass("app")
-		      	$(this).addClass("app")
-	      		// 右边全部隐藏
-	      		$preview.hide()
-	      		$preview.removeClass("focus")
-	      		a = $(this).attr("index")
-		      	// 鼠标移上的这个右边显示
-		      	$preview.eq(a).fadeIn(100)
-		      	$preview.eq(a).addClass("focus")
-		  	})
-      	})
+      	$(".home_leftcol a").mouseenter(function(){
+	  		var $aLeft = $(this).closest(".home_leftcol").children("a")
+	  		for(var i = 0;i<$aLeft.length;i++){
+	  			$aLeft.eq(i).attr("index",i)
+	  		}
+			if(!$(this).hasClass("app")){
+				var $focus = $(this).closest(".tab_nav").find(".home_rightcol .focus")
+				var $liList = $(this).closest(".tab_nav").find(".home_rightcol li")
+				a = $(this).attr("index")
+				$aLeft.removeClass("app") 
+				$focus.fadeOut(200,function(){
+					$liList.hide()
+					$liList.removeClass("focus")
+					for(var b=0;b<$liList.length;b++){
+						$liList.eq(b).stop()
+					}
+					$liList.eq(a).fadeIn(200)
+					$liList.eq(a).addClass("focus")
+				})
+				$(this).addClass("app")
+			}
+		})
+		
+
+      	var $merchandise = $(".merchandise").find("li")
+      	$(".tab_nav").eq(0).addClass("prev")
       	// 选项卡鼠标点击事件
       	$merchandise.click(function(){
+      		var c = 0
       		// 枚举选项卡个数
       		for(var i = 0;i<$merchandise.length;i++){
+      			$merchandise.eq(i).attr("index",i)
+      			c = $(this).attr("index")
       			// 全部隐藏
       			$merchandise.eq(i).removeClass("tab_content")
-      			$tab_nav.eq(i).removeClass("prev")
+      			$(".tab_nav").eq(i).removeClass("prev")
       		}
-      		// 判断当前点击的内容
-      		if($(this).html() == "新品与热门商品"){
-      			// 让当前的显示
-      			$merchandise.eq(0).addClass("tab_content")
-      			$tab_nav.eq(0).addClass("prev")
-      		}
-      		// 判断当前点击的内容
-      		if($(this).html() == "热销商品"){
-      			// 让当前的显示
-      			$merchandise.eq(1).addClass("tab_content")
-      			$tab_nav.eq(1).addClass("prev")
-      		}
-      		// 判断当前点击的内容
-      		if($(this).html() == "即将推出"){
-      			// 让当前的显示
-      			$merchandise.eq(2).addClass("tab_content")
-      			$tab_nav.eq(2).addClass("prev")
-      		}
-      		// 判断当前点击的内容
-      		if($(this).html() == "优惠"){
-      			// 让当前的显示
-      			$merchandise.eq(3).addClass("tab_content")
-      			$tab_nav.eq(3).addClass("prev")
+      		$(".home_leftcol").children("a").removeClass("app")
+      		$(".home_leftcol").eq(c).children("a").eq(0).addClass("app")
+      		$(".home_rightcol").children("li").removeClass("focus")
+      		$(".home_rightcol").eq(c).children("li").eq(0).show()
+      		$(".home_rightcol").eq(c).children("li").eq(0).addClass("focus")
+      		// // 判断当前点击的li非法数字
+      		if($(this).attr("index") == c){
+      			// 让当前的li显示
+      			$merchandise.eq(c).addClass("tab_content")
+      			// 当前li对应的盒子显示
+      			$(".tab_nav").eq(c).addClass("prev")
       		}
       	})
 
-      	// 弹出层逻辑
-		var $home_maincap = $(".home_maincap")   // 第一个轮播图对应的弹出层
-		var b = 0
+
+      	// --------------------------------弹出层逻辑-----------------------------
+
 		// 鼠标移到第一个轮播图上时
 		$("#chart1 .scheme").mouseenter(function(){
+			var n = $(this).attr("index")
+			var b = 0
 			// 枚举侧边弹出层个数
-			for(var i = 0;i<$home_maincap.length;i++){
+			for(var i = 0;i < $(".home_maincap").length;i++){
 				// 弹出层全部隐藏
-				$home_maincap.eq(i).hide()
+				$(".home_maincap").eq(i).hide()
 			}
 			// 当前显示的图片对应的弹出层显示
-			$home_maincap.eq(n).fadeIn(200)
-			$substanceImg = $home_maincap.eq(n).find("img")
+			$(".home_maincap").eq(n).fadeIn(200)
+			$substanceImg = $(".home_maincap").eq(n).find("img")
 			$substanceImg.eq(0).show()
 			// 开启计时器，隔两秒换一张图
 			pop = setInterval(function(){
@@ -222,7 +245,7 @@
 			},1500)
 		}).mouseleave(function(){ 
 			// 弹出层隐藏 
-			$home_maincap.hide()
+			$(".home_maincap").hide()
 			// 关闭计时器
 			clearInterval(pop)
 			for(var i=0;i<$substanceImg.length;i++){
@@ -232,13 +255,15 @@
 			$substanceImg.eq(b).fadeIn(300)
 		})
 		
-		// 倒计时逻辑
-		var spotlight = document.getElementById("spotlight")	
+
+		// --------------------------------倒计时函数-----------------------------
+
+		// 倒计时函数
 		function daojishi(){
 			var nowTime = new Date()
 			var overTime = new Date()
-			var ri = overTime.setDate(22)
-			var month = overTime.setMonth(1)
+			var ri = overTime.setDate(1)
+			var month = overTime.setMonth(2)
 			var hour = overTime.setHours(18)
 			var minute = overTime.setMinutes(0)
 			var second = overTime.setSeconds(0)
@@ -247,10 +272,6 @@
 			var h = Math.floor((miao-86400*t)/3600)
 			var m = Math.floor((miao-86400*t-3600*h)/60)
 			var s = Math.floor(miao-86400*t-3600*h-60*m)
-			// 判断天数是否小于10
-			if(t<10){
-				t = "0"+t
-			}
 			// 判断小时是否小于10
 			if(h<10){
 				h = "0"+h
@@ -263,45 +284,43 @@
 			if(s<10){
 				s = "0"+s
 			}
-			spotlight.innerHTML = t+" 天 "+h+"时"+m+"分"+
-			s+"秒"
+			$("#spotlight").html(t+" 天 "+h+"时"+m+"分"+
+			s+"秒")
 		}
 		// 开启计时器
 		setInterval(function(){
 			daojishi()
 		},1000)
 
-		var autoplay = setInterval(function(){
-			rightAuto($("#chart1 .scheme"),$("#chart1 .dot span"))
-		},3000)
-		$(".box").mouseenter(function(){
-			clearInterval(autoplay)
-		})
-		$(".box").mouseleave(function(){
-			autoplay = setInterval(function(){
-				rightAuto($("#chart1 .scheme"),$("#chart1 .dot span"))
-			},3000)
-		})
 
-		var $screenshots = $(".screenshots")
-		var $contentImg = $(".contentImg").find("li")
-		var $screenImg = $(".screenshots").find("img")
-		var $firstFigure = $(".contentImg").find(".firstFigure")
+		// ----------------------轮播图点右边左边对应显示-------------------------
+
+		//  第一个轮播图点右边左边对应显示
+		var $contentImg = $(".contentImg").find("li")    // 左边的图片
+		var $screenImg = $(".screenshots").find("img")   // 右边的图片
+		var $firstFigure = $(".contentImg").find(".firstFigure")  // 左边显示的第一张
 		var m = 0
-		var a = 0
+		// 右边图片鼠标移上
 		$screenImg.mouseenter(function(){
+			// 枚举左边所有li里的图片
 			for(var i=0;i < $contentImg.length;i++){
+				// 左边所有li里的图片加上非法数字
 				$contentImg.eq(i).attr("index",i)
+				// 右边所有img加上非法数字
 				$screenImg.eq(i).attr("index",i)
-				a = $screenshots.eq(i).attr("index",i)
 				m = $(this).attr("index")
 			}
+			// 左边显示的第一张全部隐藏
 			$firstFigure.hide()
+			// 图片矫正到正确位置
 			$contentImg.eq(m).fadeIn(200)
+			// 鼠标离开右边图片
 		}).mouseleave(function(){
+			// 左边显示的这一张隐藏
 			$contentImg.eq(m).hide()
 		})
-
-		$screenshots.mouseleave(function(){
+		// 鼠标离开右边图片的大盒子
+		$(".screenshots").mouseleave(function(){
+			// 左边的第一张全部显示
 			$firstFigure.show()
 		})
